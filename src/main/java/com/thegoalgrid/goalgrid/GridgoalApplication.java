09b024e8babe65648c1ca9102f8b2660c9e735b9
Arthur Wood <cwarthur1029@gmail.com>
2025-02-02 23:05:19 -0800

    Update timezone

--- src/main/java/com/thegoalgrid/goalgrid/GridgoalApplication.java
+++ src/main/java/com/thegoalgrid/goalgrid/GridgoalApplication.java
@@ -1,13 +1,20 @@
 package com.thegoalgrid.goalgrid;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class GridgoalApplication {
 
+	@PostConstruct
+	public void init() {
+		// Set JVM default time zone to UTC
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GridgoalApplication.class, args);
 	}
-
 }
